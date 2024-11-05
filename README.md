4bbe56f7182f57adcba5f835eb0bf7c009e11207
Nastya Orlova <web.orlova.dev@gmail.com>
2024-11-05 13:02:47 +0300

    Вывод карточек из массива(js), функция создания карточке и функция удаление

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/NastyshaOrlova/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// @todo: Темплейт карточки/Получение щаблона
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// @todo: DOM узлы/Получение элементов страницы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData);
+  placesList.append(card);
+});
